37a066e00f5f974af192b5445fa8c0328bae9296
James Dias <jamesdias0@gmail.com>
2018-01-18 19:41:00 -0300

    Added authentication with devise, config routes root and i18n devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+	protected
+
+	  def after_sign_in_path_for(current_user)
+	    root_path
+	  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: 'James',
+				kind: 1,
+	            email: 'jamesdias0@gmail.com', password: 123456, password_confirmation: 123456)
