1f1a64fe29d040082c9ebd113b6d9f624974442c
James Dias <jamesdias0@gmail.com>
2018-01-18 19:16:32 -0300

    Added views with bootstrap and config i18n

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,11 @@ module SysOnline
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    #Fix erro: "enforce_available_locales!"
+    I18n.enforce_available_locales = false
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    # Set default locale to something other than :en
+    I18n.default_locale = :"pt-BR"
   end
 end
